5dffb93de72fada56d17d3886632950187e86945
Mulpeter91 <robertmulpetr@gmail.com>
2022-06-29 18:29:43 +0100

    Adding application flow

--- Homely/Models/UserAccount.cs
+++ Homely/Models/UserAccount.cs
@@ -6,7 +6,7 @@
         public Roles? Role { get; set; }
         public DateTime? Created { get; set; }
         public PersonalInformation? PersonalInformation { get; set; }
-        public List<Application>? Applications { get; set; }
+        public List<UserApplication>? UserApplications { get; set; }
     }
 
     public class PersonalInformation
@@ -15,17 +15,30 @@
         public string? Surname { get; set; }
         public string? Email { get; set; }
         public string? ProfilePicture { get; set; }
+        public string? DefaultBio { get; set; }
+        public string? LinkedInUrl { get; set; }
+        public string? InstagramUrl { get; set; }
     }
 
-    public class Application
+    public class UserApplication
     {
+        public Guid? Id { get; set; }
         public Guid? PropertyId { get; set; }
-        public DateTime? Created { get; set; }
+        public DateTime? Submitted { get; set; }
+        public UserApplicationStatus Status { get; set; }
+        public string SubmittedBio { get; set; }
     }
 
     public enum Roles
     {
         User,
         Admin
+    }
+
+    public enum UserApplicationStatus
+    {
+        Draft,
+        Active,
+        Inactive
     }
 }
